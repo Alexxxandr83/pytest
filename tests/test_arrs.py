9172dbefe64e1302c891cacfc24bda703338f117
Alexandp L <alexandp83@mail.ru>
2023-04-02 18:54:39 +0300

    покрыл тестами как смог))

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,6 +1,7 @@
 from utils import arrs
 import pytest
 
+
 def test_get():
     with pytest.raises(IndexError):
         assert arrs.get([1, 2, 3], 1, "test") == 2
@@ -10,3 +11,6 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 1) == []
+    assert arrs.my_slice([1, 2, 3], -1) == [3]
+    assert arrs.my_slice([1, 2, 3], -4) == [1, 2, 3]
